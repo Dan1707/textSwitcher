d0153ab980850cfe9956d13413933c9dccf3b922
Dan1707 <danuhae@gmail.com>
2025-04-02 09:54:40 +0300

    added 'themeSwitcher' component

---
+++ src/app/home/components/themeSwitcher.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { Moon, Sun } from 'lucide-vue-next'
+import Button from '@/shared/components/ui/button/Button.vue'
+import { ref } from 'vue'
+
+const switchTheme = ref(true)
+
+const toggleTheme = () => {
+	const page = document.querySelector('html')
+
+	switchTheme.value = !switchTheme.value
+	page?.classList.toggle('dark')
+}
+</script>
+
+<template>
+	<button @click="toggleTheme">
+		<Button class="rounded-full w-10 h-10 flex items-center justify-center">
+			<Sun v-if="switchTheme" />
+			<Moon v-else />
+		</Button>
+	</button>
+</template>
--- src/app/home/views/homeView.vue
+++ src/app/home/views/homeView.vue
@@ -1,18 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
 import FormFields from '../components/formFields.vue'
 import FormSelect from '../components/formSelect.vue'
-import { Moon, Sun } from 'lucide-vue-next'
-import Button from '@/shared/components/ui/button/Button.vue'
-
-const switchTheme = ref(true)
-
-const toggleTheme = () => {
-	const page = document.querySelector('html')
-
-	switchTheme.value = !switchTheme.value
-	page?.classList.toggle('dark')
-}
+import ThemeSwitcher from '../components/themeSwitcher.vue'
 </script>
 
 <template>
@@ -22,12 +11,7 @@ const toggleTheme = () => {
 				<p class="text-primary font-light font-mono text-lg">Hi!, Daniel</p>
 				<h1 class="text-3xl font-black">Let's switch some text today!</h1>
 			</div>
-			<button @click="toggleTheme">
-				<Button class="rounded-full w-10 h-10 flex items-center justify-center">
-					<Sun v-if="switchTheme" />
-					<Moon v-else />
-				</Button>
-			</button>
+			<ThemeSwitcher />
 		</div>
 		<div class="mt-5">
 			<FormSelect />
